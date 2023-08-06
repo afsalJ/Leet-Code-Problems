f053f511ec202e2b1bbd4fad39fbfcc1766a98b1
MOHAMED AFSAL J <99009790+afsalJ@users.noreply.github.com>
2023-08-06 07:27:50 +0530

    Update Peak Index in a mountain array.java

--- Peak Index in a mountain array.java
+++ Peak Index in a mountain array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int[] temp=Arrays.copyOf(arr, arr.length);;
+        int[] temp=Arrays.copyOf(arr, arr.length);
         Arrays.sort(temp);
         int max=temp[temp.length-1];
         for(int i=0;i<arr.length;i++)
